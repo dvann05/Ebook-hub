25676b5a33a933e9420f449d493501dc032e95d9
dvann05 <151783881+dvann05@users.noreply.github.com>
2026-07-26 13:21:42 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,3 +25,52 @@ function closeSidebar(){
     sidebar.classList.remove("active");
     overlay.classList.remove("active");
 }
+
+/* ===========================
+   USER PROFILE
+=========================== */
+
+const nama = localStorage.getItem("ebookhub_username");
+
+const greeting = document.getElementById("greeting");
+const userName = document.getElementById("userName");
+const avatar = document.getElementById("userAvatar");
+
+if (nama) {
+
+    userName.textContent = nama;
+
+    avatar.textContent =
+        nama.charAt(0).toUpperCase();
+
+    const jam = new Date().getHours();
+
+    let sapaan = "Selamat Datang";
+
+    if (jam >= 5 && jam < 11) {
+
+        sapaan = "☀️ Selamat Pagi";
+
+    }
+
+    else if (jam >= 11 && jam < 15) {
+
+        sapaan = "🌤️ Selamat Siang";
+
+    }
+
+    else if (jam >= 15 && jam < 18) {
+
+        sapaan = "🌇 Selamat Sore";
+
+    }
+
+    else {
+
+        sapaan = "🌙 Selamat Malam";
+
+    }
+
+    greeting.textContent = sapaan;
+
+}
