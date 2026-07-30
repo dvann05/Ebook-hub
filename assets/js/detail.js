6cebcac0933d201efa8dc5f7c620eb0785394d53
dvann05 <151783881+dvann05@users.noreply.github.com>
2026-07-31 05:21:50 +0700

    Remove advertisement script from detail.js
    Removed advertisement script and related code from detail.js.

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -64,21 +64,7 @@ if (ebook) {
     document.getElementById("downloadBtn").href =
     `../download.html?id=${ebook.id}`;
 
-}
-
-   <script>
-  atOptions = {
-    'key' : '69fcd5fd56a30742013741f1be4d3c19',
-    'format' : 'iframe',
-    'height' : 50,
-    'width' : 320,
-    'params' : {}
-  };
-</script>
-<script src="https://www.highperformanceformat.com/69fcd5fd56a30742013741f1be4d3c19/invoke.js"></script>
-
-   
-else {
+} else {
 
     document.querySelector("main").innerHTML = `
 
