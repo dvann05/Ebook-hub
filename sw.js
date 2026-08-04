4b0359879b35e843ad9703c7d386d3a4a0d96fca
dvann05 <151783881+dvann05@users.noreply.github.com>
2026-08-05 01:50:06 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,47 +1,6 @@
-const CACHE_NAME = "ebookhub-v1";
-
-const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/404.html",
-    "/offline.html",
-    "/about.html",
-    "/category.html",
-    "/contact.html",
-    "/detail.html",
-    "/downloads.html",
-    "/privacy.html",
-    "/terms.html",
-    "/assets/css/style.css"
-];
-
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
-    );
-});
-
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.map(key => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            )
-        )
-    );
-});
-
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        fetch(event.request)
-            .catch(() =>
-                caches.match(event.request)
-                    .then(response => response || caches.match("/offline.html"))
-            )
-    );
-});
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11502584
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
